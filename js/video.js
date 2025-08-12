436789c0d78450958504d9bf8cfe9fdda74c6651
nothing36 <prodipsarker953@gmail.com>
2025-08-12 21:53:33 +0600

    third commit

--- js/video.js
+++ js/video.js
@@ -1,1 +1,20 @@
-console.log('js working');
+//fetch , load, show categories
+
+//create loadCategories
+const loadCategories = () =>{
+//fetch
+fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+.then((res)=> res.json())
+.then((data) => displayCategories(data.categories))
+.catch((error) => console.log(error));
+
+};
+
+const displayCategories = (categories) =>{
+// console.log('load catergories');
+// console.log(data.categories)
+categories.forEach((item) => {
+    console.log(item);
+});
+};
+loadCategories();
